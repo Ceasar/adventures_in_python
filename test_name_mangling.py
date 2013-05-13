dff6817a54e9be29e95b2f4267dfefb24ce24cb2
Ceasar Bautista <cbautista2010@gmail.com>
2013-05-13 09:10:17 -0700

    add name mangling stuff

---
+++ test_name_mangling.py
@@ -0,0 +1,30 @@
+"""
+Demonstrate the strange interaction between hidden methods and subclassing.
+"""
+ 
+ 
+class Foo(object):
+    def __do_something(self):
+        print "Foo is doing something"
+ 
+    def trigger(self):
+        self.__do_something()
+ 
+ 
+class Bar(Foo):
+    def __do_something(self):
+        print "Bar is doing something"
+ 
+ 
+class FooBar(Foo):
+    def __do_something(self):
+        print "Bar is doing something"
+ 
+    def trigger(self):
+        self.__do_something()
+ 
+ 
+ 
+Foo().trigger() # Prints "Foo is doing something"
+Bar().trigger() # Prints "Foo is doing something"
+FooBar().trigger() # Prints "Bar is doing something"
