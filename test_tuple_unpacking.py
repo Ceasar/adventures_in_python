7b0136d87f2fd4be290286d1786e04e3158a85e9
Ceasar Bautista <cbautista2010@gmail.com>
2013-05-28 23:24:45 -0700

    more tuple unpacking tricks

--- test_tuple_unpacking.py
+++ test_tuple_unpacking.py
@@ -7,6 +7,60 @@ a, b = 1, 2
 assert a == 1
 assert b == 2
 
+# Note it is actually assignment
+
+o = [1, 2]
+
+o[0], o[1] = 2, 1
+
+assert o == [2, 1]
+
+# This can be useful and strange
+
+n = range(10)
+
+n[5:] = range(5)
+
+assert n == range(5) * 2
+
+m = range(10)
+
+m[0:10:2] = range(5)
+
+assert m == [0, 1, 1, 3, 2, 5, 3, 7, 4, 9]
+
+# things that are too long will override and append
+
+aa = range(10)
+
+aa[5:] = range(10)
+
+assert aa == range(5) + range(10)
+
+# we can also splice
+
+ab = range(10)
+
+ab[5:5] = range(10)
+
+assert ab == range(5) + range(10) + range(5, 10)
+
+# overwriting only happens in the designated range
+
+ac = range(10)
+
+ac[5:6] = range(10)
+
+assert ac == range(5) + range(10) + range(6, 10)
+
+# another way of effectively calling `list`
+
+k = []
+
+k[:] = 'abc'
+
+assert k == ['a', 'b', 'c']
+
 # Tuples can be unpacked structurally by mirroring form carefully
 
 c, (d) = (1, (2,))
@@ -56,6 +110,13 @@ assert q == 1
 # {p} = {1}
 # Can't assign to literal
 
+# a curiosity
+
+[] = []
+[] = {}
+[] = ()
+[] = ''
+
 # Names can be swapped through multiple assignment
 
 g, h = 1, 2
@@ -75,3 +136,16 @@ j, k = k, j = 1, 2
 
 assert j == 2
 assert k == 1
+
+# We can exploit this
+
+k, k[:] = [], 'abc'
+
+assert k == ['a', 'b', 'c']
+
+# Functions can match tuples, but not anything else
+
+def foo(a, (b, c), d):
+    return a, b, c, d
+
+assert foo(1, (2, 3), 4) == (1, 2, 3, 4)
