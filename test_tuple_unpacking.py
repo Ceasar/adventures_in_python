859e3a5e8a044d84bc4d24e2153380ccd998d736
Ceasar Bautista <cbautista2010@gmail.com>
2013-05-28 22:17:50 -0700

    add tuple-unpacking

---
+++ test_tuple_unpacking.py
@@ -0,0 +1,62 @@
+"""
+Behavior of tuple unpacking is fun.
+"""
+
+a, b = 1, 2
+
+assert a == 1
+assert b == 2
+
+# Tuples can be unpacked structurally by mirroring form carefully
+
+c, (d) = (1, (2,))
+
+assert c == 1
+assert d == (2,)
+
+e, (f,) = (1, (2,))
+
+assert e == 1
+assert f == 2
+
+u, [v] = [0, [1]]
+
+assert u == 0
+assert v == 1
+
+s, t = 'ab'
+
+assert s == 'a'
+assert t == 'b'
+
+# Note, Sets are sorted before unpacking
+
+y, z = {1, 2}
+
+assert y == 1
+assert z == 2
+
+w, x = {1, 0}
+
+assert w == 0
+assert x == 1
+
+# Names can be swapped through multiple assignment
+
+g, h = 1, 2
+
+g, h = h, g
+
+assert g == 2
+assert h == 1
+
+# Names are assigned left to right
+
+i, i = 1, 2
+
+assert i == 2
+
+j, k = k, j = 1, 2
+
+assert j == 2
+assert k == 1
