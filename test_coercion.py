d281376171fedabaf50644bed55e8b893dd24102
Ceasar Bautista <cbautista2010@gmail.com>
2013-06-04 23:28:59 -0700

    simplify test_coercion

--- test_coercion.py
+++ test_coercion.py
@@ -4,39 +4,30 @@
 # In ``x + y``, if *x* is a sequence that implements sequence concatenation,
 # sequence concatenation is invoked.
 
-
-xs = [1, 2, 3]
-
-assert xs + xs == [1, 2, 3, 1, 2, 3]
-
-ys = (1, 2, 3)
-
-assert ys + ys == (1, 2, 3, 1, 2, 3)
-
-zs = '123'
-
-assert zs + zs == '123123'
-
+assert [1, 2, 3] + [1, 2, 3] == [1, 2, 3, 1, 2, 3]
+assert (1, 2, 3) + (1, 2, 3) == (1, 2, 3, 1, 2, 3)
+assert '123' + '123' == '123123'
 
 # In ``x * y``, if one operator is a sequence that implements sequence
 # repetition, and the other is an integer, sequence repetition is invoked.
 
-
-assert xs * 2 == [1, 2, 3, 1, 2, 3]
-assert ys * 2 == (1, 2, 3, 1, 2, 3)
-assert zs * 2 == '123123'
+assert [1, 2, 3] * 2 == [1, 2, 3, 1, 2, 3]
+assert (1, 2, 3) * 2 == (1, 2, 3, 1, 2, 3)
+assert '123' * 2 == '123123'
 
 # NOTE: The order of operands is irrelevant.
 
-assert 2 * xs == xs * 2
+assert 2 * [1, 2, 3] == [1, 2, 3] * 2
 
 # NOTE: Booleans are considered integers. Therefore, multiplication by booleans
 # is permissible.
 
 assert True == 1
+assert [1, 2, 3] * True == [1, 2, 3]
 
-assert xs * True == xs
-assert xs * False == []
+# Multiplying by 0 or False gives empty sequences
+
+assert [1, 2, 3] * False == [1, 2, 3] * -1 == []
 
 # Rich comparisons (implemented by method ``__eq__()`` and so on) never use
 # coercion.
